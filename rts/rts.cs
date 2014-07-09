7bec70fae0478cf5c96288e0c6228a4c6413f31c
Robert <rstr5105@gmail.com>
2014-07-08 23:58:56 -0700

    Hello World!

---
+++ rts/rts.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rts
+{
+    class rts
+    {
+        static int Main(string[] args)
+        {
+            System.Console.WriteLine("Hello World");
+            for (int i = 0; i < 10; i++)
+            {
+                System.Console.WriteLine(i);
+            }
+                return 0;
+        }
+    }
+}
